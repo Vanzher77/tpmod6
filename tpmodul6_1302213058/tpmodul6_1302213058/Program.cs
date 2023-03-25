a0ba5d51fa8cbd6d4358ba8ad2c2b243e1c09173
Vanzher77 <121426519+Vanzher77@users.noreply.github.com>
2023-03-26 03:09:38 +0700

    implementasi design by contract

--- tpmodul6_1302213058/tpmodul6_1302213058/Program.cs
+++ tpmodul6_1302213058/tpmodul6_1302213058/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 
 class program
@@ -6,7 +8,11 @@ class program
     public static void Main(string[] args)
     {
         SayaTubeVideo tube = new SayaTubeVideo("Tutorial Design By Contract - Mochamad Rizky Evan Ramadhan");
-        tube.IncreasePlayCount(0);
+       
+         for(int i = 0; i < 100; i++)
+         {
+             tube.IncreasePlayCount(10000000);
+         }
         tube.PrintVideoDetails();
     }
 }
@@ -19,20 +25,27 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string t)
     {
+        Debug.Assert(t.Length <= 100 && t != null, "judul video memiliki panjang maksimal 100 karakter dan tidak berupa null.");        
+        //melakukan generate secara random sepanjang 5 digit dengan di isi angka acak di antara 10000, 99999
         var random = new Random();
         this.id = random.Next(10000, 99999);
         playCount = 0;
         title = t;
+       
         
     }
 
     public void IncreasePlayCount(int p)
     {
+        //memastikan kondisi apakah sesuai dengan inputan apa tidak
+        Debug.Assert( p <= 10000000, "Input penambahan play count maksimal 10.000.000 per panggilan method-nya.");
         //block try and catch untuk meminimalisir error code, fungsi try untuk mencoba apakah ada error pada code atau tidak
         //fungsi catch untuk menangkap code yang tidak bisa di convert
         try
         {
-            int playcount = (int)p;
+
+            //checked melakukan exception error pada overflow
+            checked { this.playCount += p; }
         }
         catch(OverflowException error)
         {
@@ -42,8 +55,9 @@ public class SayaTubeVideo
     
     public void PrintVideoDetails()
     {
+        //melakukan pemanggilan id,title dan playCount
         Console.WriteLine("Id : "  + this.id);
-        Console.WriteLine("Tittle : " + this.title);
+        Console.WriteLine("Title : " + this.title);
         Console.WriteLine("PlayCount : " + this.playCount);
     }
 }
